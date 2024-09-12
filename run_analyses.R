3a13e4be0eceb7dc5da958e05893e954072da187
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-09-12 16:18:23 +0100

    minor edit

--- run_analyses.R
+++ run_analyses.R
@@ -4,10 +4,13 @@
 source("src/03_load_packages.R")
 
 ##-------------- Analysis --------------##
+
+## main analyses
 source("src/11_generate_inputs.R")
 source("src/12_generate_analyses.R")
 source("src/13_generate_multipliers.R")
 
+## sensitivity analysese
 source("src/15_sensitivity_analysis_reallocation.R")
 source("src/16_sensitivity_analysis_io_table.R")
 
