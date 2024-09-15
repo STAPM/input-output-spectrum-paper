aec878b936779e46b4a11e5f1519459105a0072b
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-09-15 10:50:31 +0100

    update sensitivity analysis code

--- run_analyses.R
+++ run_analyses.R
@@ -10,9 +10,10 @@ source("src/11_generate_inputs.R")
 source("src/12_generate_analyses.R")
 source("src/13_generate_multipliers.R")
 
-## sensitivity analysese
-source("src/15_sensitivity_analysis_reallocation.R")
-source("src/16_sensitivity_analysis_io_table.R")
+## sensitivity analyses
+source("src/15_sensitivity_analysis_1_io_table.R")
+source("src/16_sensitivity_analysis_2_reallocation_rate.R")
+source("src/17_sensitivity_analysis_3_reallocation_target.R")
 
 ##-------------- Outputs --------------##
 source("src/25_final_results_tables.R")
---
+++ src/17_sensitivity_analysis_reallocation_target.R
@@ -0,0 +1,205 @@
+
+## This code runs the four scenarios while varying the input-output table used
+
+# (i)   alcohol (-10% all alcohol)
+# (ii)  tobacco (-10% all tobacco, licit and illicit)
+# (iii) food (-10% confectionary)
+# (iv)  gambling (-10% gross gambling yield)
+
+######################
+### prepare inputs ###
+
+## fixed inputs
+year <- 2019
+year_io <- 2019
+reallocate_prop <- 1.0
+reallocate_food <- NULL
+consumption_category_vec <- 1:36
+
+## scenario-varying inputs (list objects with one vector of expenditure changes per scenario)
+change_food            <- list(rep(-0.0, 19), rep(-0.0, 19), c(rep(-0.0, 18),-0.1), rep(-0.0, 19))
+change_gambling        <- list(rep(-0.0, 9),  rep(-0.0, 9),  rep(-0.0, 9),  rep(-0.1, 9))
+change_tobacco_licit   <- list(rep(-0.0, 2),  rep(-0.1, 2),  rep(-0.0, 2),  rep(-0.0, 2))
+change_tobacco_illicit <- list(rep(-0.0, 2),  rep(-0.1, 2),  rep(-0.0, 2),  rep(-0.0, 2))
+change_alcohol         <- list(rep(-0.1, 4),  rep(-0.0, 4),  rep(-0.0, 4),  rep(-0.0, 4))
+
+excluded_products      <- c("alcohol","tobacco","food","gambling")
+
+
+##############
+### launch ###
+
+## loop over the four scenarios and reallocation proportions
+
+for (i in 1:4){
+  for (j in 1:length(consumption_category_vec)){
+
+    policy <- cdohio(year = year,
+                     year_io = year_io,
+                     reallocate_prop = reallocate_prop,
+                     excluded_products = excluded_products[i],
+                     change_food = change_food[[i]],
+                     change_gambling = change_gambling[[i]],
+                     change_tobacco_licit = change_tobacco_licit[[i]],
+                     change_tobacco_illicit = change_tobacco_illicit[[i]],
+                     change_alcohol = change_alcohol[[i]],
+                     reallocate_food = reallocate_food[[i]],
+                     consumption_category = consumption_category_vec[j])
+
+    effects_pct <- policy$effects_pct
+
+    ## remove gross earnings and keep only the full effect
+    effects_pct[, earn_vec := NULL]
+    effects_pct <- effects_pct[type == "Direct + Indirect + Induced", ]
+
+    effects_pct <- melt(effects_pct,
+                        id.vars = "type",
+                        value.name = "estimate_rel",
+                        variable.name = "outcome")
+
+    effects_pct[, type := NULL]
+
+    effects_pct[, policy := excluded_products[i]]
+    effects_pct[, consumption_category := consumption_category_vec[j]]
+
+
+
+    if (j == 1){
+      sa3_result <- copy(effects_pct)
+    } else {
+      sa3_result <- rbindlist(list(sa3_result, effects_pct))
+    }
+
+  }
+
+  assign(paste0("sa3_result_",excluded_products[i]),sa3_result)
+  rm(sa3_result)
+}
+
+sa3_result <- rbindlist(list(sa3_result_alcohol, sa3_result_tobacco, sa3_result_food, sa3_result_gambling))
+rm(sa3_result_alcohol, sa3_result_tobacco, sa3_result_food, sa3_result_gambling)
+
+##############################################
+#### Plot the sensitivity analysis results
+
+sa3_result[, outcome := factor(outcome,
+                               levels = c("output_vec","gva_vec","tax_vec","fte_vec","net_earn_vec","inc_tax_nics_vec"),
+                               labels = c("Output","Gross Value Added","Tax on Employers","Employment","Net Earnings","Tax on Employees"))]
+
+sa3_result[, policy := factor(policy,
+                              levels = c("alcohol","tobacco","food","gambling"),
+                              labels = c("(1) alcohol", "(2) tobacco", "(3) food", "(4) gambling"))]
+
+#############################################
+## Calculate all effects relative to 2019
+
+ggplot(sa3_result[outcome == "Output"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_Output.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_Output.pdf", width = 8, height = 6)
+
+ggplot(sa3_result[outcome == "Gross Value Added"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_GVA.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_GVA.pdf", width = 8, height = 6)
+
+ggplot(sa3_result[outcome == "Tax on Employers"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_TaxEmployers.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_TaxEmployers.pdf", width = 8, height = 6)
+
+
+ggplot(sa3_result[outcome == "Employment"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_Employment.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_Employment.pdf", width = 8, height = 6)
+
+
+ggplot(sa3_result[outcome == "Net Earnings"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_NetEarnings.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_NetEarnings.pdf", width = 8, height = 6)
+
+
+ggplot(sa3_result[outcome == "Tax on Employees"]) +
+  aes(x = consumption_category, y = estimate_rel/100, fill = policy) +
+  facet_wrap(~policy, ncol = 2, scales = "free_y") +
+  geom_col(position = "dodge", colour = "black") +
+  geom_hline(yintercept = 0, colour = "black", linewidth = 1, linetype = 2) +
+  theme_minimal() +
+  #theme_classic() +
+  labs(y = "Direct + Indirect + Induced Effect",
+       x = "Consumption category",
+       fill = "scenario")  +
+  scale_y_continuous(labels = scales::percent) +
+  theme( # remove the vertical grid lines
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()) +
+  scale_fill_manual(values = c("#fb8b24","#e36414","#9a031e","#5f0f40"))
+ggsave("output/FIG_SA3_consumption_category_IncTaxes.jpg", width = 8, height = 6)
+ggsave("output/FIG_SA3_consumption_category_IncTaxes.pdf", width = 8, height = 6)
