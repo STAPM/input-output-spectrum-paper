c41de3260bbbaea8b5280b45d1bc4a5662248bad
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-10-17 14:22:51 +0100

    update the metafile

--- run_analyses.R
+++ run_analyses.R
@@ -1,6 +1,7 @@
 ## This meta-file runs all R code to produce the analysis
 
 ##-------------- Preamble --------------##
+#source("src/01_install_packages.R") # install the model
 source("src/03_load_packages.R")
 
 ##-------------- Analysis --------------##
@@ -9,6 +10,7 @@ source("src/03_load_packages.R")
 source("src/11_generate_inputs.R")
 source("src/12_generate_analyses.R")
 source("src/13_generate_multipliers.R")
+source("src/14_multipliers_and_reallocated_spending.R")
 
 ## sensitivity analyses
 source("src/15_sensitivity_analysis_io_table.R")
